151520d306a75bf5bd334a8b81c247707c743cbf
Vladislav Egorov <vlad.45.45@mail.ru>
2021-07-02 20:03:11 +0700

    Add adaptive theme

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,21 @@
 <template>
-  <h1>No boobas</h1>
+  <div class="boobas-container">
+    <h1 class="boobas">No boobas</h1>
+    <span><nuxt-link to="/boobas">Go to boobas</nuxt-link></span>
+  </div>
 </template>
 
-<script lang="ts">
+<script lang="js">
 import Vue from 'vue'
 
 export default Vue.extend({})
 </script>
+<style scoped>
+.boobas-container {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+</style>
 
